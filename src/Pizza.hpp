0fe5e01e98cf088c66072195b10f6c5e61da7513
O Elman <oelman@localhost.localdomain>
2021-05-13 23:43:28 +0200

    switching structure of pizza a bit + order

--- src/Pizza.hpp
+++ src/Pizza.hpp
@@ -1,6 +1,8 @@
 #ifndef PIZZA_HPP_
 #define PIZZA_HPP_
 
+#include <string>
+
 enum class PizzaType
 {
     Regina = 1,
@@ -22,6 +24,23 @@ struct Pizza
 {
     PizzaType _type;
     PizzaSize _size;
+    //int _order_nb;
+    //int _cooking_time;
+    //std::string _order_code;
 };
+
+/*
+struct Ingredient
+{
+    int _something = 5;
+};
+
+struct KMetaData
+{
+    int busy_cooks;
+    Ingredient _ingredient;
+    int _ownId;
+};*/
+
 
 #endif //PIZZA_HPP_
--- src/Reception.hpp
+++ src/Reception.hpp
@@ -12,54 +12,34 @@
 #include "Messenger.hpp"
 #include "Pizza.hpp"
 
-/*
-enum PizzaType
-{
-    Regina = 1,
-    Margarita = 2,
-    Americana = 4,
-    Fantasia = 8
-};
 
-enum PizzaSize
-{
-    S = 1,
-    M = 2,
-    L = 4,
-    XL = 8,
-    XXL = 16
-};
+class Reception {
+public:
+    Reception(float, int, int);
 
-typedef struct {
-    PizzaType _type;
-    PizzaSize _size;
-} order_t;
-*/
-    class Reception {
-    public:
-        Reception(float, int, int);
+    std::vector<Order> parse(const std::string &str);
 
-        std::vector<Order> parse(const std::string &str);
-
-        void run(); /* MAIN LOOP */
-        void getInput();
+    void run(); /* MAIN LOOP */
+    void getInput();
 
 //private:
 //    PizzaType strToPizzaType(std::string str);
 //    PizzaSize strToPizzaSize(std::string str);
 
-    private:
-        float _multiply;
-        int _cooks;
-        int _refill;
-        std::vector<Order> _order;
-        int _nbKitchens;
-        std::unique_ptr<Messenger> messenger;
-    private:
-        void addKitchen();
-        void runWindow();
+private:
+    float _multiply;
+    int _cooks;
+    int _refill;
+    //std::vector<KMetaData> _kitchen_mds;
+    //std::vector<Pizza> _pizza_todo;
+    std::vector<Order> _orders;
+    int _nbKitchens;
+    std::unique_ptr<Messenger> messenger;
+private:
+    void addKitchen();
+    void runWindow();
 
-    };
+};
 
 
 #endif //RECEPTION_HPP_
